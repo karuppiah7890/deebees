2623cbd07a64e3e7c3f26e5b836d925eef3d0d63
Karuppiah Natarajan <karuppiah7890@gmail.com>
2023-02-12 22:47:37 +0530

    update manual test script to use the docker container and make the test script executable

--- counter-machine/manual-test.sh
+++ counter-machine/manual-test.sh
@@ -1,13 +1,24 @@
-# Run make; ./counter-machine
+docker build -t counter-machine:dev .
+docker_container_name=counter-machine-$RANDOM
+docker run --name $docker_container_name -d --publish 8080:8080 counter-machine:dev
 
 # Initial counter value should be 0, if this script is run just after starting the server
 curl -H "content-type: application/json" -i http://localhost:8080/counter
 
+echo
+echo
+
 # Malformed JSON request should give bad request error
 curl -X PATCH -H "content-type: application/json" -i http://localhost:8080/counter -d '{"something": "}'
 
+echo
+echo
+
 # Empty request body (Malformed) JSON request should give bad request error
 curl -X PATCH -H "content-type: application/json" -i http://localhost:8080/counter
+
+echo
+echo
 
 # Sending random key-value pairs or any dummy data in the JSON request should not give any errors (for now)
 # TODO: Maybe we gotta change this behavior and not accept random stuff the user sends.
@@ -18,18 +29,38 @@ curl -X PATCH -H "content-type: application/json" -i http://localhost:8080/count
 # is not there for patch request, hmm
 curl -X PATCH -H "content-type: application/json" -i localhost:8080/counter -d '{"something": "okay"}'
 
+echo
+echo
+
 # What happens for this case? Does it ignore the value? No. It gives 400 bad request due to bad data type.
 # But it gives a very wierd error like -
 # "json: cannot unmarshal string into Go struct field JsonRequest.incrementBy of type int"
 # Gotta make the error look good!
 curl -X PATCH -H "content-type: application/json" -i localhost:8080/counter -d '{"incrementBy": "okay"}'
 
+echo
+echo
+
 curl -X PATCH -H "content-type: application/json" -i localhost:8080/counter -d '{"incrementBy": 100 }'
+
+echo
+echo
 
 # Now the counter value should be 100 :)
 curl -H "content-type: application/json" -i http://localhost:8080/counter
 
+echo
+echo
+
 curl -X PATCH -H "content-type: application/json" -i localhost:8080/counter -d '{"incrementBy": 100 }'
+
+echo
+echo
 
 # Now the counter value should be 200 now :)
 curl -H "content-type: application/json" -i http://localhost:8080/counter
+
+echo
+echo
+
+docker rm -f $docker_container_name
